1a8df36505edd507a580b0cce00468028cc520d8
berkcansavur <berkcansavur@gmail.com>
2024-03-06 10:57:39 +0300

    cli is working -from now on appalaizer command iz working from terminal

---
+++ cli.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+const path = require('path');
+import { ProjectTreeService } from './src/services';
+
+
+const inputPath = process.cwd(); // Input Directory
+const outputDir = path.join(inputPath, 'docs'); // Output Directory
+
+const projectTreeService = new ProjectTreeService();
+projectTreeService.generateProjectTree(inputPath, outputDir);
+
+console.log("Project tree generated successfully.");
+
+
